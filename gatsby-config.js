b981c4a8ddba213527dd745abe2c28f666e372dc
Kate Levin <kllevin@gmail.com>
2017-11-18 14:04:32 +1100

    adding contentful source

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `s6hj5d8j3nqc`,
+        accessToken: `1e8fa0664f24f1f57446aa2168c5cca3cd1bc382cec3e980dd41f9c2d085c60a`,
+      },
+    },
+  ],
 }
